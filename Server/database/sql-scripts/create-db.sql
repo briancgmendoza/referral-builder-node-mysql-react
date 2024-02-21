df7cc57b86583938ef882d2d92c5b9971a4034d4
Brian <brian.mendoza@betterask.erni>
2024-02-21 20:02:58 +0800

    Creating userdb script and inserting dummy data

---
+++ Server/database/sql-scripts/create-db.sql
@@ -0,0 +1,25 @@
+-- Create Database if not exists
+CREATE DATABASE IF NOT EXISTS `userdb`;
+
+-- Use the university database
+USE `userdb`;
+
+-- Create Courses table
+CREATE TABLE IF NOT EXISTS `UserProfile` (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    given_name VARCHAR(50),
+    surname VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    phone VARCHAR(20),
+    house_no VARCHAR(20),
+    street VARCHAR(100),
+    suburb VARCHAR(100),
+    state VARCHAR(50),
+    postcode VARCHAR(20),
+    country VARCHAR(50),
+    avatar_image BLOB
+);
+
+INSERT INTO UserProfile (given_name, surname, email, phone, house_no, street, suburb, state, postcode, country, avatar_image) 
+VALUES 
+('Brian Christopher', 'Mendoza', 'briancgmendoza@gmail.com', '+639773722289', '3966', 'Main St', 'Anytown', 'Stateful', '00001', 'PH', NULL)
