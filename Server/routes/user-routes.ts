55aadaf39eb796211803f75f4449b250e28a611e
Brian <brian.mendoza@betterask.erni>
2024-02-21 21:30:25 +0800

    Delete user route

--- Server/routes/user-routes.ts
+++ Server/routes/user-routes.ts
@@ -4,5 +4,6 @@ import * as usersController from "../controllers/user.controller"
 
 export default (router: Router) => {
     router.get("/users", usersController.getUsers);
-    router.post("/add-user", usersController.addUser)
+    router.post("/add-user", usersController.addUser);
+    router.delete("/user/:userId", usersController.deleteUser)
 };
