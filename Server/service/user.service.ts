955c808d264b8b1a7bd2182dc6ea646a317d9b12
Brian <brian.mendoza@betterask.erni>
2024-02-21 21:30:03 +0800

    Delete user service

--- Server/service/user.service.ts
+++ Server/service/user.service.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { FieldPacket, ResultSetHeader } from "mysql2/promise";
 
 import { pool } from "../controllers/db.controller"
 import { TUsers, TUserProfile } from './../types/index';
@@ -70,5 +71,28 @@ export const addUserService = async (req: Request, res: Response): Promise<void>
   } catch (error) {
     console.error('Error adding user:', error);
     res.status(400).json({ error: 'Invalid data provided. Please check your input.' });
+  }
+};
+
+export const deleteUserService = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+
+    if (!userId) {
+      res.status(400).json({ error: 'User ID is required.' });
+      return;
+    }
+
+    const [result]: [ResultSetHeader, FieldPacket[]] = await pool.query('DELETE FROM UserProfile WHERE user_id = ?', [userId]);
+
+    if (result.affectedRows === 0) {
+      res.status(404).json({ error: `User with ID ${userId} not found.` });
+      return;
+    }
+
+    res.status(200).json({ message: `User with ID ${userId} deleted successfully.` });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
