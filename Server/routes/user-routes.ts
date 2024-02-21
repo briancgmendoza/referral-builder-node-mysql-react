6b16a24ec2e49b04c85ee99afdaa39e18b381645
Brian <brian.mendoza@betterask.erni>
2024-02-21 23:47:38 +0800

    Update user route

--- Server/routes/user-routes.ts
+++ Server/routes/user-routes.ts
@@ -6,4 +6,5 @@ export default (router: Router) => {
     router.get("/users", usersController.getUsers);
     router.post("/add-user", usersController.addUser);
     router.delete("/user/:userId", usersController.deleteUser)
+    router.put("/update-user/:userId", usersController.updateUser)
 };
