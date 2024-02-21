55c2ac6792b65668bb23233c533853dd1f2ab032
Brian <brian.mendoza@betterask.erni>
2024-02-21 21:11:48 +0800

    Add user service

--- Server/service/user.service.ts
+++ Server/service/user.service.ts
@@ -1,5 +1,8 @@
-import { TUsers } from './../types/index';
+import { Request, Response } from "express"
+
 import { pool } from "../controllers/db.controller"
+import { TUsers, TUserProfile } from './../types/index';
+
 
 export const getUsersService = async (): Promise<TUsers[]> => {
   try {
@@ -8,5 +11,64 @@ export const getUsersService = async (): Promise<TUsers[]> => {
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
+  }
+};
+
+export const addUserService = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      given_name,
+      surname,
+      email,
+      phone,
+      house_no,
+      street,
+      suburb,
+      state,
+      postcode,
+      country
+    }: TUserProfile = req.body;
+
+
+    const requiredFields = [
+      'given_name',
+      'surname',
+      'email',
+      'phone',
+      'house_no',
+      'street',
+      'suburb',
+      'state',
+      'postcode',
+      'country'
+    ];
+
+    if (requiredFields.some(field => !req.body[field])) {
+      res.status(400).json({ error: 'Missing field. Please check your input.' });
+      return;
+    }
+
+    const query = `
+      INSERT INTO UserProfile (given_name, surname, email, phone, house_no, street, suburb, state, postcode, country)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    await pool.query(query, [
+      given_name,
+      surname,
+      email,
+      phone,
+      house_no,
+      street,
+      suburb,
+      state,
+      postcode,
+      country
+    ]);
+
+    res.status(201).json({ message: 'User added successfully' });
+  } catch (error) {
+    console.error('Error adding user:', error);
+    res.status(400).json({ error: 'Invalid data provided. Please check your input.' });
   }
 };
