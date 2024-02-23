846216db8922693e0669ec4e8a05d162054cef96
Brian <brian.mendoza@betterask.erni>
2024-02-23 12:32:23 +0800

    Updating db creation script

--- Server/database/sql-scripts/create-db.sql
+++ Server/database/sql-scripts/create-db.sql
@@ -4,22 +4,23 @@ CREATE DATABASE IF NOT EXISTS `userdb`;
 -- Use the university database
 USE `userdb`;
 
--- Create Courses table
+-- Create UserProfile table
 CREATE TABLE IF NOT EXISTS `UserProfile` (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     given_name VARCHAR(50),
     surname VARCHAR(50),
     email VARCHAR(100) UNIQUE,
-    phone VARCHAR(20),
+    phone VARCHAR(15),
     house_no VARCHAR(20),
     street VARCHAR(100),
     suburb VARCHAR(100),
     state VARCHAR(50),
     postcode VARCHAR(20),
     country VARCHAR(50),
-    avatar_image BLOB
+    avatar_image VARCHAR(255)
 );
 
+-- Inserting dummy data
 INSERT INTO UserProfile (given_name, surname, email, phone, house_no, street, suburb, state, postcode, country, avatar_image) 
 VALUES 
-('Brian Christopher', 'Mendoza', 'briancgmendoza@gmail.com', '+639773722289', '3966', 'Main St', 'Anytown', 'Stateful', '00001', 'PH', NULL)
+('Brian Christopher', 'Mendoza', 'briancgmendoza@gmail.com', '639773722289', '3966', 'Main St', 'Anytown', 'Stateful', '00001', 'PH', NULL);
